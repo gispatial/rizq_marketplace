64b07f17a73a0fc2b85782922bace08e3ada7d77
GiSpatial Technology Malaysia <react7tech@gmail.com>
2020-11-30 00:20:32 +0800

    pushy-mart

---
+++ src/config/api.js
@@ -0,0 +1,10 @@
+export const API = 'https://rizq.com.my/';
+
+export const CONSUMER_KEY = 'ck_ff40f2a76a2bf44ccca1d6ca41ba7c5aa1b99316';
+export const CONSUMER_SECRET = 'cs_5fcd6c0426b574e0d783adc73a5ceb6c016c04e2';
+
+export default {
+  API_ENDPOINT: API,
+  CONSUMER_KEY,
+  CONSUMER_SECRET,
+};
---
+++ src/config/onesignal.js
@@ -0,0 +1,1 @@
+export const APP_ID = 'd4e396e3-bc91-450b-b23f-e7fa23bcc051';
